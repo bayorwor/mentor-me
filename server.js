96dc4f66a52c7a08c2b65f7bacdfc5da4cd30ad7
Bayorwor <bayorwor60@gmail.com>
2022-05-18 16:07:34 +0000

    video feature

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("colors");
 const app = express();
 const { connectDB } = require("./configs/db");
 const { errorHandler, notFound } = require("./middlewares/error.middleware");
+const server = require("http").createServer(app);
 
 //routes
 const { mentorsRouter } = require("./routes/mentor.routes");
@@ -12,6 +13,13 @@ const { usersRoute } = require("./routes/user.routes");
 const { bookingRouter } = require("./routes/bookings.routes");
 
 connectDB();
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +36,22 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`.rainbow);
 });
